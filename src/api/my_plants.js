3920f6a37773eb607e379a72faccf0f318c91dbf
Dylan Pavelko <dylanpavelko@users.noreply.github.com>
2021-07-14 23:31:07 -0700

    plant instances on plant detail view now shows kind of

--- src/api/my_plants.js
+++ src/api/my_plants.js
@@ -3,3 +3,9 @@ import { get } from './fetch';
 export const getMyPlants = () => {
   return get('/my_plants_api');
 };
+
+export const getPlant = (plant_id) => {
+  return get('/plants/' + plant_id + ".json");
+};
+
+  
--- src/components/ResourceLink.js
+++ src/components/ResourceLink.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { Linking, View, Text, TouchableOpacity } from 'react-native';
+import { Linking, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function ResourceLink(props) {
 
     const uri = (props.link);
 
     return (
-      <View>
-      <TouchableOpacity
-        onPress={(event) => Linking.openURL(uri)} ><Text>{props.name}</Text></TouchableOpacity>
+      <View style={styles.button} >
+        <TouchableOpacity
+          onPress={(event) => Linking.openURL(uri)} >
+          <View>
+          <Text style={{color:'white'}}>{props.name}</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
 
 }
+
+const styles = StyleSheet.create({
+  button:{
+    backgroundColor:"#18cd58",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    margin: 5,
+    padding: 10,
+    
+  },
+});
